b24ffa5a8706df04bef3a5b112aeb7e59cc83e61
r79559 <seuss@fullsail.edu>
2013-02-20 13:52:50 -0500

    Created Character Objects in JSON

---
+++ miller_susan_assignment3/miller_susan_json.js
@@ -0,0 +1,60 @@
+// alert("JavaScript works!");
+
+// Author: Susan R. Miller
+// Project: Assignment 3
+// Course: SDI 1302
+// Title: JSON Data
+
+var characters = {
+    "goodGuys": [
+
+        {
+            "name": "Dorothy",
+            "need": "a home",
+            "characteristic": "beautiful voice"
+        },
+        {
+            "name": "The Scarecrow",
+            "need": "a brain",
+            "characteristic": "straw hair"
+        },
+        {
+            "name": "The Tin Man",
+            "need": "a heart",
+            "characteristic": "funnel hat"
+        },
+        {
+            "name": "The Cowardly Lion",
+            "need": "the nerve",
+            "characteristic": "fluffy mane"
+        },
+        {
+            "name": "Munchkins",
+            "from": "Munchkinland",
+            "number": 100,
+            "characteristic": "red cheeks"
+        },
+        {
+            "name": "Glinda the Good Witch",
+            "direction": "North",
+            "characteristic": "magical bubble"
+        }
+    ],
+
+    "badGuys": [
+
+        {
+            "name": "Flying Monkeys",
+            "number": 6,
+            "characteristic": "creepy wings"
+        },
+        {
+            "name": "Wicked Witch",
+            "direction": "West",
+            "characteristic": "green pallor"
+        },
+        {
+            "name": "The Great and Powerful Oz"
+        }
+    ]
+};
