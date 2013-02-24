4488f2dab1a16c26b34a9f860ef8010a1b0d5007
r79559 <seuss@fullsail.edu>
2013-02-24 11:53:23 -0500

    phoneTest function built

--- miller_susan_assignment4/miller_susan_assignment4.js
+++ miller_susan_assignment4/miller_susan_assignment4.js
@@ -4,3 +4,19 @@
 // Course: SDI 1302
 // Assignment #4: Code Library
 
+//  Does a string follow a 123-456-7890 pattern like a phone number?
+
+var phoneTest = function (phoneNumber) {
+    if ((phoneNumber.charAt(3) === "-") && (phoneNumber.charAt(7) === "-")) {
+        validNumber = true;
+        console.log("Thank you for entering a valid phone number.");
+        return validNumber;
+    } else {
+        validNumber = false;
+        console.log("Please enter a valid phone number using xxx-xxx-xxxx format.");
+        return validNumber;
+    }
+};
+
+
+//  Does a string follow an aaa@bbb.ccc pattern like an email address?
