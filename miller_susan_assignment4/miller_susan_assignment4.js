36d6e2166c0447bb378bc3b487d3986d0c8ffd02
r79559 <seuss@fullsail.edu>
2013-02-25 13:41:17 -0500

    Final clean-up for readability

--- miller_susan_assignment4/miller_susan_assignment4.js
+++ miller_susan_assignment4/miller_susan_assignment4.js
@@ -3,23 +3,26 @@
 // Author: Susan R. Miller
 // Course: SDI 1302
 // Assignment #4: Code Library
-
+// Notes: console.logs can all be removed after grading.  They are present within just to show full functionality of the functions.
+//        functions with similar purpose (ie. number analysis) can be combined into objects for simplicity of call after grading.
 
 // **** STRING FUNCTIONS **** //
 
 
 //  1. Does a string follow a 123-456-7890 pattern like a phone number?
+
 console.log("Does a string follow a 123-456-7890 pattern like a phone number?");
+
 
 var phoneNumber = {
     isValidNumber: function (phoneNumber) {
         if ((phoneNumber.charAt(3) === "-") && (phoneNumber.charAt(7) === "-")) {
             var validNumber = true;
-            console.log("Thank you for entering a valid phone number, " + phoneNumber + ".");                           // can be removed after grading
+            console.log("Thank you for entering a valid phone number, " + phoneNumber + ".");
             return validNumber;
         } else {
             var validNumber = false;
-            console.log(phoneNumber + " is not a valid phone number. Please enter your number using xxx-xxx-xxxx format.");      // can be removed after grading
+            console.log(phoneNumber + " is not a valid phone number. Please enter your number using xxx-xxx-xxxx format.");
             return validNumber;
         }
     }
@@ -28,19 +31,23 @@ var phoneNumber = {
 phoneNumber.isValidNumber("212-555-1212");
 phoneNumber.isValidNumber("212.555.1212");
 
+
+
 //  2. Does a string follow an aaa@bbb.ccc pattern like an email address?
+
 console.log("******************************************************************************************************");
 console.log("2. Does a string follow an aaa@bbb.ccc pattern like an email address?");
+
 
 var emailAddress = {
     isValidEmail: function (email) {
         if (((email.charAt(email.length - 4) === ".") || (email.charAt(email.lentgh - 3 === "."))) && (email.indexOf("@") >= 0)) {
             var validEmail = true;
-            console.log(email + " appears to be a valid email address.");                                               // can be removed after grading
+            console.log(email + " appears to be a valid email address.");
             return validEmail;
         } else {
             var validEmail = false;
-            console.log(email + " does not appear to be a valid email address.  Please try again.");                    // can be removed after grading
+            console.log(email + " does not appear to be a valid email address.  Please try again.");
             return validEmail;
         }
     }
@@ -50,19 +57,23 @@ emailAddress.isValidEmail("satr@mac.com");
 emailAddress.isValidEmail("satr@kiwi.co.nz");
 emailAddress.isValidEmail("donkeykong.com");
 
+
+
 // 3. Is the string a URL? (Does it start with http: or https:?)
+
 console.log("******************************************************************************************************");
 console.log("3. Is the string a URL? (Does it start with http: or https:?)");
+
 
 var url = {
     isValidUrl: function (url) {
 
         if (url.startsWith("http://") || url.startsWith("https://")) {
             var validUrl = true;
-            console.log(url + " appears to be a valid URL.");                                                           // can be removed after grading
+            console.log(url + " appears to be a valid URL.");
         } else {
             var validUrl = false;
-            console.log(url + " does not appear to be a valid URL.  Please use http:// or https:// format.");           // can be removed after grading
+            console.log(url + " does not appear to be a valid URL.  Please use http:// or https:// format.");
         }
         return validUrl;
     }
@@ -73,9 +84,12 @@ url.isValidUrl("https://www.domain.com");
 url.isValidUrl("www.domain.com");
 
 
+
 //  4. Title-case a string (split into words, then uppercase the first letter of each word)
+
 console.log("******************************************************************************************************");
 console.log("4. Title-case a string (split into words, then uppercase the first letter of each word)");
+
 
 var caseManip = {
     titleCase: function (title) {
@@ -90,7 +104,7 @@ var caseManip = {
                 titled.push(newWord);                                                    // Adds now capped word into array
             }
         var cappedTitle = titled.join(" ");                                              // Joins words in array together with a space between
-        console.log(cappedTitle);                                                                                       // can be removed after grading
+        console.log(cappedTitle);
         return cappedTitle;
     }
 };
@@ -98,16 +112,20 @@ var caseManip = {
 caseManip.titleCase("it's a dog eat dog world, and I'm wearing milkbone underwear.");
 
 
+
 //   5. Given a string that is a list of things separated by a given string, as well as another string separator,
 //      return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
+
 console.log("******************************************************************************************************");
 console.log("5. Given a string that is a list of things separated by a given string, as well as another string separator,");
 console.log("   return a string with the first separator changed to the second: \"a,b,c\" + \",\" + \"/\" → \"a/b/c\".\"");
+
+
 var stringSeparator = {
     switch: function (string, originalSeparator, newSeparator) {
         var words = string.split(originalSeparator);
         var newString = words.join(newSeparator);
-        console.log("\"" + string + "\" is now \"" + newString + ".\"");                                                // can be removed after grading
+        console.log("\"" + string + "\" is now \"" + newString + ".\"");
         return newString;
     }
 };
@@ -115,17 +133,19 @@ var stringSeparator = {
 stringSeparator.switch("dogs, cats, fish, bones", ",", " -");
 
 
+
 // **** NUMBER FUNCTIONS **** //
 
-
 //  6. Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+
 console.log("******************************************************************************************************");
 console.log("6. Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10");
+
 
 var decimalPlace = {
     setToTwo: function (number) {
         var moneyNum = number.toFixed(2);
-        console.log(number + " is now " + moneyNum);                                                                    // can be removed after grading
+        console.log(number + " is now " + moneyNum);
         return moneyNum;
     }
 };
@@ -135,19 +155,22 @@ decimalPlace.setToTwo(3.14159);
 decimalPlace.setToTwo(796.309);
 
 
+
 //  7. Fuzzy-match a number: is the number above or below a number within a certain percent?
+
 console.log("******************************************************************************************************");
 console.log("7. Fuzzy-match a number: is the number above or below a number within a certain percent?");
+
 
 var numberAnalysis = {
     fuzzyMatch: function (a, b, decimalPercent) {
         var diff = Math.abs(a - b);
         if (((diff/a) <= decimalPercent) || ((diff/b) <= decimalPercent)) {
             var withinPercent = true;
-            console.log(a + " and " + b + " are within " + decimalPercent + "% of each other.");                        // can be removed after grading
+            console.log(a + " and " + b + " are within " + decimalPercent + "% of each other.");
         } else {
             var withinPercent = false;
-            console.log(a + " and " + b + " are not within " + decimalPercent + "% of each other.");                    // can be removed after grading
+            console.log(a + " and " + b + " are not within " + decimalPercent + "% of each other.");
         }
         return withinPercent;
     }
@@ -157,9 +180,12 @@ numberAnalysis.fuzzyMatch(36,42,.15);
 numberAnalysis.fuzzyMatch(75,23,.55);
 
 
+
 //  8. Find the number of hours or days difference between two dates.
+
 console.log("******************************************************************************************************");
 console.log("8. Find the number of hours or days difference between two dates.");
+
 
 var dateAnalysis = {
     dateDiff: function (var1, var2, hoursOrDays) {
@@ -171,7 +197,7 @@ var dateAnalysis = {
                 } else if (hoursOrDays === "days") {
                     var difference = Math.abs(elapsed / 1000 / 60 / 60 / 24);
                 }
-            console.log("The difference between " + var1 + " and " + var2 + " is " + difference.toFixed(2) + " " + hoursOrDays);        // can be removed after grading
+            console.log("The difference between " + var1 + " and " + var2 + " is " + difference.toFixed(2) + " " + hoursOrDays);
         return difference;
     }
 
@@ -184,19 +210,22 @@ dateAnalysis.dateDiff("May 17, 1977 10:59 AM", "July 13, 2005 7:26 PM", "days");
 dateAnalysis.dateDiff("May 17, 1977 10:59 AM", "July 13, 2005 7:26 PM", "hours");
 
 
+
 //  9. Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+
 console.log("******************************************************************************************************");
 console.log("9. Given a string version of a number such as \"42\", return the value as an actual Number, such as 42.");
 
-var numberAnalysis2 = {                                     // will be added to numberAnalysis object for final library - separated for ease here.
+
+var numberAnalysis2 = {
     stringToNum: function (string) {
         var goAhead = isNaN(string);
         if (goAhead === false) {
             number = Number(string);
-            console.log("The string " + string + " has been converted to the number " + number + ".");                  // can be removed after grading
+            console.log("The string " + string + " has been converted to the number " + number + ".");
             return number;
         } else {
-            console.log("The string " + string + " is not a valid number.  No conversion was done.");                   // can be removed after grading
+            console.log("The string " + string + " is not a valid number.  No conversion was done.");
             return string;
         }
     }
@@ -208,29 +237,36 @@ numberAnalysis2.stringToNum("42");
 numberAnalysis2.stringToNum("Bob");
 
 
+
 // **** ARRAY FUNCTIONS **** //
 
 //  10. Find the smallest value in an array that is greater than a given number
+
 console.log("******************************************************************************************************");
 console.log("10. Find the smallest value in an array that is greater than a given number");
 
+
 var arrayAnalysis = {
     arrayValue: function (array, value) {
-        array.push(value);                           // Adds the given value to the array
-        array.sort(function (a,b) {return a - b;});  // Sorts the array numerically
-        itemsLess = array.slice(0,array.indexOf(value));              // Finds the new index of the initial value to be measured by
-        console.log(itemsLess + " are less than " + value);                                                             // can be removed after grading
+        array.push(value);                                              // Adds the given value to the array
+        array.sort(function (a,b) {return a - b;});                     // Sorts the array numerically
+        itemsLess = array.slice(0,array.indexOf(value));                // Finds the new index of the initial value to be measured by
+        console.log(itemsLess + " are less than " + value);
         return itemsLess;
     }
 }
 
 arrayAnalysis.arrayValue([5, 17, 11, 4, 7, 13], 7);
 
+
+
 //  11. Find the total value of just the numbers in an array, even if some of the items are not numbers.
+
 console.log("******************************************************************************************************");
 console.log("11. Find the total value of just the numbers in an array, even if some of the items are not numbers.");
 
-var arrayAnalysis2 = {                              // Can be combined into above arrayAnalysis object above after assignment graded
+
+var arrayAnalysis2 = {
     arrayTotal: function (array) {
         var total = 0;
         for (x = 0; x < array.length; x++) {
@@ -242,17 +278,22 @@ var arrayAnalysis2 = {                              // Can be combined into abov
                 }
             }
         }
-        console.log("The total of the numbers in your array is: " + total);                                             // can be removed after grading
+        console.log("The total of the numbers in your array is: " + total);
     }
 };
 
 arrayAnalysis2.arrayTotal([15, 37, "dog", false, "cat", 9, true]);
 
+
+
+
 //  12. Given an array of objects and the name of a key, return the array sorted by the value of that key
 //      in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
+
 console.log("******************************************************************************************************");
 console.log("12. Given an array of objects and the name of a key, return the array sorted by the value of that key");
 console.log("    in each of the objects: \"a\" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].");
+
 
 var arraySort = {
     keySort: function (array, key) {
