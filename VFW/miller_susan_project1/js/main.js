ba897ca7048a4e75193ecb58e3736e755845f432
r79559 <seuss@fullsail.edu>
2013-03-12 20:55:23 -0400

    set vars for ids in main.js

---
+++ VFW/miller_susan_project1/js/main.js
@@ -0,0 +1,13 @@
+//
+// Author: Susan R. Miller
+// Course: VFW 1303
+// Week 2
+//
+
+var choreType = document.getElementById("choretype");
+var whoDo = document.getElementById("person");
+var dateDue = document.getElementById("duedate");
+var howHard = document.getElementById("difficulty");
+var notDone = document.getElementById("no");
+var itsDone = document.getElementById("yes");
+
