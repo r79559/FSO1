58e1ce72d9946b760883a8c3310b44997196a8e5
r79559 <seuss@fullsail.edu>
2013-03-14 22:02:36 -0400

    Still baffled.  Trying to dig out.

--- VFW/miller_susan_project1/js/main.js
+++ VFW/miller_susan_project1/js/main.js
@@ -4,91 +4,43 @@
 // Week 2
 //
 
-var main = document.getElementById("content");
+// assures DOM loaded
+window.addEventListener("DOMContentLoaded", function() {
 
-// form info variables
-var type = document.getElementById("choretype");
-var who = document.getElementById("person");
-var date = document.getElementById("duedate");
-var effort = document.getElementById("difficulty");
-var done = document.getElementById("chores").complete;
-var reset = document.getElementById("resetAll");
+    // getElementById Function
+    function $(x){
+        var theElement = document.getElementbyId(x);
+        return theElement;
+    }
 
+var main = $("content");
 
-// capture functions
-var captureChore = function (){
-    localStorage.setItem("Chore Type", type.value);
-};
+// shortcut variables
+var type = $("choretype");
+var chore = $("chorename");
+var who = $("person");
+var date = $("duedate");
+var effort = $("difficulty");
+var done = $("chores").complete;
+var addChore = $("addNew");
+var showChores = $("showAll");
+var resetChores = $("resetAll");
 
-var captureWho = function (){
-    localStorage.setItem("Person", who.value);
-};
-
-var captureDate = function (){
-    localStorage.setItem("Due Date", date.value);
-};
-
-var captureEffort = function (){
-    localStorage.setItem("Difficulty", effort.value);
-};
-
-var captureDone = function (){
-    localStorage.setItem("Completed?", done.value);
-};
+var choreTypes = ["Select a Chore Type", "House", "Hygiene", "Pet", "School"];
 
 
 
-// getData functions
 
-var getData1 = function (){
-    var typeKey = localStorage.key("Chore Type");
-    var typeValue = localStorage.getItem(typeKey);
-    type.value = typeValue;
-};
-
-var getData2 = function () {
-    var whoKey = localStorage.key("Person");
-    var whoValue = localStorage.getItem(whoKey);
-    who.value = whoValue;
-};
-
-var getData3 = function () {
-    var dateKey = localStorage.key("Due Date");
-    var dateValue = localStorage.getItem(dateKey);
-    date.value = dateValue;
-};
-
-var getData4 = function () {
-    var effortKey = localStorage.key("Difficulty");
-    var effortValue = localStorage.getItem(effortKey);
-    effort.value = effortValue;
-};
-
-var getData5 = function () {
-    var doneKey = localStorage.key("Completed?");
-    var doneValue = localStorage.getItem(doneKey);
-    done.value = doneValue;
-};
-
-
-// other functions
-
-var clear = function () {
+var empty = function () {
     var check = confirm("Are you sure you want to clear all chores?");
     if (check) {
         localStorage.clear();
     } else {
-
+        alert("Let me know if you reconsider.");
     }
 };
 
-// getData calls
 
-getData1();
-getData2();
-getData3();
-getData4();
-getData5();
 
 
 // listeners
@@ -97,10 +49,10 @@ type.addEventListener("blur", captureChore);
 who.addEventListener("blur", captureWho);
 date.addEventListener("click", captureDate);
 effort.addEventListener("click", captureEffort);
-done.addEventListener("click", captureDone);
 
-
-reset.addEventListener("click", clear);
+addChore.addEventListener("click", saveChore);
+showChores.addEventListener("click", showAll);
+resetChores.addEventListener("click", empty);
 
 
 /*
@@ -125,6 +77,65 @@ for (i=0, j=info.length; i<j; i++) {
 console.log(newDiv);
 */
 
+/*
+// capture functions
+    var captureChore = function (){
+        localStorage.setItem("Chore Type", type.value);
+    };
+
+    var captureWho = function (){
+        localStorage.setItem("Person", who.value);
+    };
+
+    var captureDate = function (){
+        localStorage.setItem("Due Date", date.value);
+    };
+
+    var captureEffort = function (){
+        localStorage.setItem("Difficulty", effort.value);
+    };
+
+
+
+
+// getData functions
+
+    var getData1 = function (){
+        var typeKey = localStorage.key("Chore Type");
+        var typeValue = localStorage.getItem(typeKey);
+        type.value = typeValue;
+    };
+
+    var getData2 = function () {
+        var whoKey = localStorage.key("Person");
+        var whoValue = localStorage.getItem(whoKey);
+        who.value = whoValue;
+    };
+
+    var getData3 = function () {
+        var dateKey = localStorage.key("Due Date");
+        var dateValue = localStorage.getItem(dateKey);
+        date.value = dateValue;
+    };
+
+    var getData4 = function () {
+        var effortKey = localStorage.key("Difficulty");
+        var effortValue = localStorage.getItem(effortKey);
+        effort.value = effortValue;
+    };
+
+
+// getData calls
+
+    getData1();
+    getData2();
+    getData3();
+    getData4();
+
+// other functions
+*/
 
 
 // Confirm clearing the list.
+
+});
