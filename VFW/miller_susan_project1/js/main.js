1a1ef0b5ce34842e0afae4b6d13d76a7ce856c40
r79559 <seuss@fullsail.edu>
2013-03-14 23:22:41 -0400

    More frustration.  Things work, then break, then work.  Going to have to set aside and submit incomplete.  Will continue to work tomorrow just to figure out how to get it done.

--- VFW/miller_susan_project1/js/main.js
+++ VFW/miller_susan_project1/js/main.js
@@ -7,6 +7,7 @@
 // assures DOM loaded
 window.addEventListener("DOMContentLoaded", function() {
 
+
     // getElementById Function
     function $(x){
         var theElement = document.getElementById(x);
@@ -29,25 +30,15 @@ window.addEventListener("DOMContentLoaded", function() {
         selectLi.appendChild(makeSelect);
     }
 
-    var main = $("content");
-
-    var choreTypes = ["Select a Chore Type", "House", "Hygiene", "Pet", "School"];
+    var choreTypes = ["Select a Chore Type", "House", "Hygiene", "Pet", "School"],
+        doneValue;
     listTypes();
 
     // shortcut variables
-    var type = $("choretype");
-    var chore = $("chorename");
-    var who = $("person");
-    var date = $("duedate");
-    var effort = $("difficulty");
-    var done = $("chores").complete;
+
     var addChore = $("addNew");
     var showChores = $("showAll");
     var resetChores = $("resetAll");
-
-
-
-
 
     var empty = function () {
         var check = confirm("Are you sure you want to clear all chores?");
@@ -58,18 +49,41 @@ window.addEventListener("DOMContentLoaded", function() {
         }
     };
 
+    var radioCheck = function(){
+        var radios = document.forms[0].done;
+        for (var i = 0; i<radios.length; i++) {
+            if (radios[i].checked){
+                doneValue = radios[i].value;
+            }
+        }
+    };
 
+    var saveChore = function () {
+        var id = Math.floor(Math.random()*1000000);
+        // Gather and store values as object with form label and value
+        radioCheck();
+        var item = {};
+            item.type = ["Chore Type: ", $("choretype").value];
+            item.chore = ["Chore Name: ", $("chorename").value];
+            item.who = ["Person Responsible: ", $("person").value];
+            item.date = ["Date Due: ", $("duedate").value];
+            item.effort = ["Level of Difficulty: ", $("difficulty").value];
+            item.done = ["Is it Done? ", doneValue];
+        // Save to localStorage
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Your chore has been saved!");
+    };
 
 
     // listeners
 
-    type.addEventListener("blur", captureChore);
-    who.addEventListener("blur", captureWho);
-    date.addEventListener("click", captureDate);
-    effort.addEventListener("click", captureEffort);
+//    type.addEventListener("blur", captureChore);
+//    who.addEventListener("blur", captureWho);
+//    date.addEventListener("click", captureDate);
+//    effort.addEventListener("click", captureEffort);
 
     addChore.addEventListener("click", saveChore);
-    showChores.addEventListener("click", showAll);
+ //   showChores.addEventListener("click", showAll);
     resetChores.addEventListener("click", empty);
 
 
